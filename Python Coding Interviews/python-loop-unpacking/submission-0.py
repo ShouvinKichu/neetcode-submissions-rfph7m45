dc14e693a01020f474e16a0fdd204a2740ba8d0b
Shouvin.A <72617116+ShouvinKichu@users.noreply.github.com>
2026-08-22 23:20:31 +0530

    Add: python-loop-unpacking - submission-0

---
+++ Python Coding Interviews/python-loop-unpacking/submission-0.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    n,m = 0,""
+    for i,j in scores:
+        if j > n :
+            n,m = j,i
+    return m
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
